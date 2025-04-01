beb69bcdb370c0ae6058879eaccc09f0ee6cd563
iosua-pop <pop.iosua@yahoo.com>
2025-04-01 22:46:44 +0300

    temele de laborator

---
+++ Laborator/TemaLab/LabAssignment/Lab1/src/main/java/ro/mpp2024/Dummy.java
@@ -0,0 +1,7 @@
+package ro.mpp2024;
+
+public class Dummy {
+    static void printMessage(String msg) {
+        System.out.println(msg);
+    }
+}
---
+++ Laborator/TemaLab/LabAssignment/Lab1/src/main/java/ro/mpp2024/Main.java
@@ -0,0 +1,9 @@
+package ro.mpp2024;
+
+public class Main {
+    public static void main(String[] args) {
+        Dummy dummy = new Dummy();
+
+        dummy.printMessage("Bine ati venit la cursul de MPP!");
+    }
+}
